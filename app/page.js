281cae7023ac4e117831b9496f27346abfee8ee7
thanya2718 <2tlinkth@gmail.com>
2026-06-07 21:13:54 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -136,7 +136,7 @@ export default function VandaNichadaWebsite() {
   const homeImages = [
     "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=1000&q=80",
     "https://images.unsplash.com/photo-1600566753190-17f0baa2a6c3?auto=format&fit=crop&w=1000&q=80",
-    "https://images.unsplash.com/photo-1600607687644-c7171b42498b?auto=format&fit=crop&w=1000&q=80",
+    "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=1000&q=80",
   ];
 
   return (
